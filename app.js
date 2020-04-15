588f05e1ed179ac45bf9cfe9c87c28c7b512330a
ChoukseyKhushbu <khushbu25chouksey@gmail.com>
2020-04-16 01:11:58 +0530

    heroku deploy

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", loginRouter);
 app.use("/login", loginRouter);
 app.use("/menu", menuRouter);
 app.use("/game", gameRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
+const loginCheck = require("../middlewares/loginCheck");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  console.log(req.session.name);
-  if (req.session.name) {
-    res.send(`Hello ${req.session.name}`);
-  } else {
-    req.session = { name: "khushbu" };
-    res.send("no user");
-  }
+router.get("/", loginCheck, function (req, res, next) {
+  // console.log(req.session.name);
+  // if (req.session.name) {
+  //   res.send(`Hello ${req.session.name}`);
+  // } else {
+  //   req.session = { name: "khushbu" };
+  //   res.send("no user");
+  // }
+  res.render("login");
 });
 
 module.exports = router;
