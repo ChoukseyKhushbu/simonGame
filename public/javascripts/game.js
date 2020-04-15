1807c67aabd2ba35e14a28c9271e673a7f4142b2
ChoukseyKhushbu <khushbu25chouksey@gmail.com>
2020-04-15 23:26:18 +0530

    improve game UX

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,12 +1,21 @@
 var userClickedPattern = []; //to store user's pattern of colour
 var gamePattern = []; //to store computer's pattern
 var level = 0; //starting
-var interval;
+var timer;
 var buttonColours = ["red", "blue", "yellow", "green"];
+var isDemonstrating = true;
+
+const timeBar = document.querySelector(".timeBar");
+const timeText = document.querySelector(".timeText");
 
 function generateNextLevel() {
   userClickedPattern = [];
   ++level;
+  $("div#level").text("level " + level);
+
+  timeBar.style.width = "calc(100% - 10px)";
+  timeText.innerHTML = level + "s";
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColours[randomNumber];
   gamePattern.push(randomChosenColor);
@@ -26,13 +35,21 @@ function startNewLevel() {
 }
 
 function userSequence() {
-  //   if (userClickedPattern.length == 0 && level != 1) {
-  //     // interval = setTimeout(() => {
-  //     //   endGame();
-  //     // }, level * 1000);
-  //   }
-  console.log(this);
-  var userChosenColor = $(this).attr("id"); // getting user clicked colour
+  if (userClickedPattern.length == 0 && level != 1) {
+    let time = level;
+    timer = setInterval(() => {
+      console.log(timeText);
+      timeBar.style.width = `calc(${(time / level) * 100}% - 10px)`;
+      timeText.innerHTML = time + "s";
+      time--;
+      if (time === -1) {
+        clearInterval(timer);
+        endGame("TIME OVER");
+      }
+    }, 1000);
+  }
+
+  var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
 
   animatePress(userChosenColor);
@@ -41,22 +58,27 @@ function userSequence() {
 }
 
 function demonstrateLevel() {
+  $("div#blockScreen").show();
   for (let i = 0; i < gamePattern.length; i++) {
     setTimeout(() => {
       playSound(gamePattern[i]);
       animatePress(gamePattern[i]);
-    }, 1500 * i);
+      if (i == gamePattern.length - 1) {
+        $("div#blockScreen").hide();
+      }
+    }, 1000 * i);
   }
 }
 
-function endGame() {
-  //   clearInterval(interval);
+function endGame(reason) {
+  clearInterval(timer);
+  $("div#gameover").text(reason);
   $("div#gameover").css("display", "flex");
   //hit POST/highscore
   $.post(
     "/highscores",
     {
-      level: level,
+      level: level - 1,
     },
     function (data, status) {
       if (data.success) {
@@ -70,16 +92,14 @@ function endGame() {
 
 function checkPattern(currentLevel) {
   if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
-    console.log("success");
-
     if (userClickedPattern.length == gamePattern.length) {
-      //   clearInterval(interval);
+      clearInterval(timer);
       generateNextLevel();
-      $("div#level").text("level " + level);
+
       $("div#start").show();
     }
   } else {
-    endGame();
+    endGame("WRONG CLICK");
   }
 }
 
