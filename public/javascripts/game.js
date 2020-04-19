91d086e14bf834e5c91bff9fd0bd0e8dabd83dda
ChoukseyKhushbu <khushbu25chouksey@gmail.com>
2020-04-20 00:07:58 +0530

    improved UI and minified clientside js

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,19 +1,18 @@
 var userClickedPattern = []; //to store user's pattern of colour
+
 var gamePattern = []; //to store computer's pattern
-var level = 0; //starting
+
+var levl = 0; //starting
+
 var timer;
 var buttonColours = ["red", "blue", "yellow", "green"];
 var isDemonstrating = true;
-
 var timeBar = document.querySelector(".timeBar");
 var timeText = document.querySelector(".timeText");
-
 generateNextLevel();
-
 $("div#start").on("click", function () {
   startNewLevel();
 });
-
 $(".btn").on("click", userSequence);
 
 function startNewLevel() {
@@ -23,12 +22,10 @@ function startNewLevel() {
 
 function generateNextLevel() {
   userClickedPattern = [];
-  ++level;
-  $("div#level").text("level " + level);
-
+  ++levl;
+  $("div#level").text("level " + levl);
   timeBar.style.width = "calc(100% - 10px)";
-  timeText.innerHTML = level + "s";
-
+  timeText.innerHTML = levl + "s";
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColours[randomNumber];
   gamePattern.push(randomChosenColor);
@@ -36,25 +33,32 @@ function generateNextLevel() {
 
 function demonstrateLevel() {
   $("div#blockScreen").show();
-  for (let i = 0; i < gamePattern.length; i++) {
-    setTimeout(() => {
+
+  var _loop = function _loop(i) {
+    setTimeout(function () {
       playSound(gamePattern[i]);
       animatePress(gamePattern[i]);
+
       if (i == gamePattern.length - 1) {
         $("div#blockScreen").hide();
       }
     }, 1000 * i);
+  };
+
+  for (var i = 0; i < gamePattern.length; i++) {
+    _loop(i);
   }
 }
 
 function userSequence() {
-  if (userClickedPattern.length == 0 && level != 1) {
-    let time = level;
-    timer = setInterval(() => {
+  if (userClickedPattern.length == 0 && levl != 1) {
+    var time = levl;
+    timer = setInterval(function () {
       console.log(timeText);
-      timeBar.style.width = `calc(${(time / level) * 100}% - 10px)`;
+      timeBar.style.width = "calc(".concat((time / levl) * 100, "% - 10px)");
       timeText.innerHTML = time + "s";
       time--;
+
       if (time === -1) {
         clearInterval(timer);
         endGame("TIME OVER");
@@ -64,7 +68,6 @@ function userSequence() {
 
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
-
   animatePress(userChosenColor);
   playSound(userChosenColor);
   checkPattern(userClickedPattern.length - 1);
@@ -75,7 +78,6 @@ function checkPattern(currentLevel) {
     if (userClickedPattern.length == gamePattern.length) {
       clearInterval(timer);
       generateNextLevel();
-
       $("div#start").show();
     }
   } else {
@@ -90,7 +92,7 @@ function playSound(name) {
 
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
-  setTimeout(() => {
+  setTimeout(function () {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
 }
@@ -98,12 +100,12 @@ function animatePress(currentColour) {
 function endGame(reason) {
   clearInterval(timer);
   $("div#gameover").text(reason);
-  $("div#gameover").css("display", "flex");
-  //hit POST/highscore
+  $("div#gameover").css("display", "flex"); //hit POST/highscore
+
   $.post(
     "/highscores",
     {
-      level: level - 1,
+      level: levl - 1,
     },
     function (data, status) {
       if (data.success) {
