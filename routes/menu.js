4e3d93c0d0ef953815dfc53e917cd51c227924e5
ChoukseyKhushbu <khushbu25chouksey@gmail.com>
2020-05-21 19:26:20 +0530

    fixed URL of highscores

--- routes/menu.js
+++ routes/menu.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const jwtCheck = require("../middlewares/jwtCheck");
+const Highscores = require("../models/highscore");
+
 const router = express.Router();
 
-router.get("/", jwtCheck, function (req, res) {
-  res.render("menu");
+router.get("/", jwtCheck, async (req, res) => {
+  const currentUserID = req.user.id;
+const userScores = await Highscores.find({userid:currentUserID}).sort({createdAt:"desc"});
+
+if(userScores.length){
+  const currentScoreID= userScores[0]._id;
+  res.render("menu",{scoreID:currentScoreID});
+}else{
+  res.render("menu",{scoreID:""});
+}
 });
 
 module.exports = router;
